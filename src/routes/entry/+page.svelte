e45d7cb9e6b763274bd23f558de20d1c841e32ea
Meciahacks <meciahacks2@svitvasad.ac.in>
2024-08-20 06:50:54 +0000

    qrscanner done

--- src/routes/entry/+page.svelte
+++ src/routes/entry/+page.svelte
@@ -1,8 +1,9 @@
 <script>	
 import {Html5QrcodeScanner,Html5QrcodeScanType} from "html5-qrcode"
 	import { onMount } from "svelte";
-
+import {pb} from '../../auth'
 let html5QrcodeScanner,decodedText
+let dt
 let config = {
         fps: 10,
         qrbox: {width: 100, height: 100},
@@ -10,11 +11,14 @@ let config = {
         supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
     };
 
-
-	const onScanSuccess=(decodedText1, decodedResult)=>{
+	const onScanSuccess=async(decodedText1, decodedResult)=>{
             console.log('****',decodedResult.toString(),'****',decodedText1)
-
             decodedText=decodedText1
+            const record = await pb.collection('team_member').getOne(decodedText, {
+                expand: 'team_details',
+            });
+            console.log(record)
+            dt=record
             // const decryptedText=CryptoJS.AES.decrypt(decodedText,"ihavesecret").toString(CryptoJS.enc.Utf8)
             // text=decryptedText
             html5QrcodeScanner.clear()          
@@ -33,12 +37,8 @@ let config = {
 
 
 
-
-
-
-
 <div>
 	<h1 class='bg-slate-800 text-white p-2 text-xl uppercase font-bold'>QR Code Scanner</h1>
-    <p class="bg-orange-800 text-white text-xl font-bold">{JSON.stringify(decodedText)}</p>
+    <p class="bg-orange-800 text-white text-xl font-bold">{JSON.stringify(dt)}</p>
 	<div id="reader" width="800"/>
 </div>
